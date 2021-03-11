14abdd3c925e13d9600962f47e18dcf5f60dfce3
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-03-11 20:27:16 -0300

    add test btn

--- src/screens/Cart/Cart.screen.js
+++ src/screens/Cart/Cart.screen.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {  View } from 'react-native'
+import {  View,Button } from 'react-native'
 
 const Cart=()=>{
     return(
         <View>
             <Text>Cart Screen</Text>
+            <Button title="comprar" onPress={()=>console.log('press')}/>
         </View>
     )
 }
