e713d54ac2b5d4665326a4cb95ffcb9d5abf20ca
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-03-12 19:46:00 -0300

    add wish page

---
+++ src/screens/Wish/Wish.screen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const WishScreen = () => {
+    return ( 
+        <View>
+            <Text>a</Text>
+        </View>
+     );
+}
+ 
+export default WishScreen;
