a9c0e3050e3d7d3b932d62ce1329cb6d1d8ad134
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-03-13 19:57:35 -0300

    init wishrouter

---
+++ src/navigation/WishRouter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WishRouter = () => {
+    return ( 
+        <View>
+            {/* TODO 
+                ROUTER STACK FOR WISH
+            */}
+        </View>
+     );
+}
+ 
+export default WishRouter;
