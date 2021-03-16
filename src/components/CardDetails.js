fce45493ee4aae0f1d279dcb7b9df72a919fd7f7
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-03-15 21:54:58 -0300

    add new carddetail component

---
+++ src/components/CardDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+
+const CardDetails = () => {
+    return ( 
+        <View>
+            <Text>Card Details</Text>
+        </View>
+     );
+}
+ 
+export default CardDetails;
